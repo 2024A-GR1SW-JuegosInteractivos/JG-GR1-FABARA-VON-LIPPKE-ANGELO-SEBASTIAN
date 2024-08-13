d5904ce050a67d76350aa55cfe35ef9609043515
Angelo Fabara <angelo.fabara@epn.edu.ec>
2024-08-13 14:46:37 -0500

    Deber03

--- 01_clases/01_proyecto/Carrito/Assets/ColliderEjemplo.cs
+++ 01_clases/01_proyecto/Carrito/Assets/ColliderEjemplo.cs
@@ -4,21 +4,43 @@ using UnityEngine;
 
 public class ColliderEjemplo : MonoBehaviour
 {
-   void OnCollisionEnter2D(Collision2D other)
-   {
-        Debug.Log("Ouch!");
-   }
-   void OnTriggerEnter2D(Collider2D other)
+   [SerializeField] private float delayDestroy = 0.5f;
+    private bool tienePaquete; //por dejecto está en false
+    [SerializeField] Color32 tienePaqueteColor = new Color32(255,0,0,25);
+    [SerializeField] Color32 noTienePaqueteColor = new Color32(0, 255, 255, 255);
+    private SpriteRenderer spriteRenderer;
+    
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void OnCollisionEnter2D(Collision2D other){
+        Debug.Log("Ouch");
+    }
+/*
+    void OnTriggerEnter2D(Collider2D other){
+            Debug.Log("Entrando En trigger");
+    }
+*/
+    void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Entrando en trigger! " );
-        if (other.tag == "Paquete")
+        if (other.tag == "Paquete" && !tienePaquete) 
         {
-            Debug.Log("Pick Paquete");
+            Debug.Log("Recogio paquete");
+            tienePaquete = true; 
+            spriteRenderer.color = tienePaqueteColor;
+            Destroy(other.gameObject, delayDestroy) ;
+            
+            
         }
 
-        if (other.tag == "Cliente")
+        if (other.tag == "Cliente" && tienePaquete)
         {   
-            Debug.Log("Cliente Get");
-        }
-    }
+            tienePaquete = false; 
+            spriteRenderer.color = noTienePaqueteColor;
+            Debug.Log("Dejo paquete");
+        }
+    }
+
 }
