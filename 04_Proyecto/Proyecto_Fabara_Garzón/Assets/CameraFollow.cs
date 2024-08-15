e9610dc961f0f48ad46ca79adab5cb9bf19249bb
Angelo Fabara <angelo.fabara@epn.edu.ec>
2024-08-14 23:23:33 -0500

    fix

--- 04_Proyecto/Proyecto_Fabara_Garzón/Assets/CameraFollow.cs
+++ 04_Proyecto/Proyecto_Fabara_Garzón/Assets/CameraFollow.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player; // Referencia al transform del jugador
-    public float yOffset = 0f; // Desplazamiento vertical de la cámara
+    public float xOffset = 0f; // Desplazamiento vertical de la cámara
 
     void Update()
     {
         if (player != null)
         {
             // Seguir al jugador solo en el eje X
-            transform.position = new Vector3(player.position.x, transform.position.y + yOffset, transform.position.z);
+            transform.position = new Vector3(player.position.x, transform.position.y + xOffset, transform.position.z);
         }
     }
 }
